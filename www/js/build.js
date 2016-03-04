1d556e0ce84572c34e753d4b9e6c31bcb5845b4e
Bobby Johnson <bobby@madnesslabs.net>
2016-03-04 01:04:00 -0600

    Finish facebook and google registration / login.

--- www/js/build.js
+++ www/js/build.js
@@ -69367,21 +69367,82 @@ var MadnessCup;
 (function (MadnessCup) {
     'use strict';
     var LoginController = (function () {
-        function LoginController($state, enjin) {
+        function LoginController($state, enjin, $firebaseObject, $firebaseArray, $rootScope) {
             this.$state = $state;
             this.enjin = enjin;
+            this.$firebaseObject = $firebaseObject;
+            this.$firebaseArray = $firebaseArray;
+            this.$rootScope = $rootScope;
             //On Load
+            this.ref = new Firebase(this.enjin.db.firebase.host);
+            this.playerUrl = this.enjin.db.firebase.host + 'player/';
         }
-        LoginController.prototype.login = function () {
-            var ref = new Firebase(this.enjin.db.firebase.host);
-            ref.authWithOAuthPopup('facebook', function (error, authData) {
+        LoginController.prototype.loginFacebook = function () {
+            this.ref.authWithOAuthPopup('facebook', function (error, authData) {
                 if (error) {
                     console.log('Login Failed!', error);
                 }
                 else {
-                    console.log('Authenticated successfully with payload:', authData);
+                    this.authenticate(authData);
                 }
+            }.bind(this));
+        };
+        LoginController.prototype.loginGoogle = function () {
+            this.ref.authWithOAuthPopup('google', function (error, authData) {
+                if (error) {
+                    console.log('Login Failed!', error);
+                }
+                else {
+                    this.authenticate(authData);
+                }
+            }.bind(this));
+        };
+        LoginController.prototype.authenticate = function (data) {
+            // if user is already registered
+            var playerRef = new Firebase(this.playerUrl + data.auth.uid);
+            this.$firebaseObject(playerRef).$loaded().then(function (player) {
+                if (player.name) {
+                    this.startSession(player);
+                }
+                else {
+                    var playersRef = new Firebase(this.playerUrl);
+                    var players = this.$firebaseArray(playersRef);
+                    var newPlayer = {
+                        name: '',
+                        avatar: '',
+                        profile: ''
+                    };
+                    switch (data.provider) {
+                        case 'google':
+                            newPlayer.name = data.google.displayName;
+                            newPlayer.avatar = data.google.profileImageURL;
+                            newPlayer.profile = data.google.cachedUserProfile.link;
+                            break;
+                        case 'facebook':
+                            newPlayer.name = data.facebook.displayName;
+                            newPlayer.avatar = data.facebook.profileImageURL;
+                            newPlayer.profile = data.facebook.cachedUserProfile.link;
+                            break;
+                        default:
+                            return false;
+                    }
+                    playersRef.child(data.auth.uid).set(newPlayer, function () {
+                        this.startSession(newPlayer);
+                    }.bind(this));
+                }
+            }.bind(this))
+                .catch(function (error) {
+                console.log(error);
             });
+        };
+        LoginController.prototype.startSession = function (player) {
+            this.enjin.session = this.$rootScope.session = {
+                name: player.name,
+                avatar: player.avatar,
+                profile: player.profile
+            };
+            localStorage.setItem('enjin_session', JSON.stringify(this.enjin.session));
+            this.$state.go('home');
         };
         return LoginController;
     }());
