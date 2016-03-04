98ebe426f9eec2823dcda4e850e83ef7ac311297
Bobby Johnson <bobby@madnesslabs.net>
2016-03-03 21:52:50 -0600

    Add image processing to build process and add images for app

--- www/js/build.js
+++ www/js/build.js
@@ -69349,27 +69349,6 @@ var MadnessCup;
 })(MadnessCup || (MadnessCup = {}));
 
 /// <reference path="../../tsd/tsd.d.ts"/>
-angular.module('MadnessCup').directive('allowPattern', function () {
-    return {
-        restrict: 'A',
-        compile: function (tElement, tAttrs) {
-            return function (scope, element, attrs) {
-                // I handle key events
-                element.bind('keypress', function (event) {
-                    var keyCode = event.which || event.keyCode; // I safely get the keyCode pressed from the event.
-                    var keyCodeChar = String.fromCharCode(keyCode); // I determine the char from the keyCode.
-                    // If the keyCode char does not match the allowed Regex Pattern, then don't allow the input into the field.
-                    if (!keyCodeChar.match(new RegExp(attrs.allowPattern, 'i'))) {
-                        event.preventDefault();
-                        return false;
-                    }
-                });
-            };
-        }
-    };
-});
-
-/// <reference path="../../tsd/tsd.d.ts"/>
 angular.module('MadnessCup').directive('setBackground', function (FallBackground) {
     return {
         restrict: 'EA',
@@ -69387,6 +69366,27 @@ angular.module('MadnessCup').directive('setBackground', function (FallBackground
             FallBackground.get(url, $scope.fallback, function (background) {
                 element[0].style.backgroundImage = background['background-image'];
             });
+        }
+    };
+});
+
+/// <reference path="../../tsd/tsd.d.ts"/>
+angular.module('MadnessCup').directive('allowPattern', function () {
+    return {
+        restrict: 'A',
+        compile: function (tElement, tAttrs) {
+            return function (scope, element, attrs) {
+                // I handle key events
+                element.bind('keypress', function (event) {
+                    var keyCode = event.which || event.keyCode; // I safely get the keyCode pressed from the event.
+                    var keyCodeChar = String.fromCharCode(keyCode); // I determine the char from the keyCode.
+                    // If the keyCode char does not match the allowed Regex Pattern, then don't allow the input into the field.
+                    if (!keyCodeChar.match(new RegExp(attrs.allowPattern, 'i'))) {
+                        event.preventDefault();
+                        return false;
+                    }
+                });
+            };
         }
     };
 });
