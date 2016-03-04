0f214c8f9780d41f538e11eab715db301e65f2cf
Emmy Johnson <sphynx245@gmail.com>
2016-03-03 20:54:38 -0600

    Add page for tournament, admin, home, and round.

--- src/ts/router.ts
+++ src/ts/router.ts
@@ -7,7 +7,19 @@ module MadnessCup {
             $stateProvider
                 .state('login', {'url':'/login', 
 'templateUrl':'html/page/login.html', 
-'controller':'MadnessCup.LoginController as ctrl'});
+'controller':'MadnessCup.LoginController as ctrl'})
+				.state('round', {'url':'/round', 
+'templateUrl':'html/page/round.html', 
+'controller':'MadnessCup.RoundController as ctrl'})
+				.state('admin', {'url':'/admin', 
+'templateUrl':'html/page/admin.html', 
+'controller':'MadnessCup.AdminController as ctrl'})
+				.state('home', {'url':'/home', 
+'templateUrl':'html/page/home.html', 
+'controller':'MadnessCup.HomeController as ctrl'})
+				.state('tournament', {'url':'/tournament', 
+'templateUrl':'html/page/tournament.html', 
+'controller':'MadnessCup.TournamentController as ctrl'});
 
             $urlRouterProvider.otherwise(function($injector, $location) {
                 var $state = $injector.get('$state');
